5aa1d853d981d252d628d0c2ea33fab69f3487d1
Roseco-crs <rodolphe.segbedji.aims.ac.rw>
2024-01-29 20:40:31 +0100

    Designed and build a retrieval part of RAG

--- RAG/MyFunctions.py
+++ RAG/MyFunctions.py
@@ -1,6 +1,20 @@
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+#from langchain.vectorstores import Chroma
+from langchain_community.vectorstores import Chroma
+from PyPDF2 import PdfReader
+from langchain_community.embeddings import HuggingFaceEmbeddings
+#from langchain.embeddings import HuggingFaceInstructEmbeddings
+#from langchain_community.embeddings import HuggingFaceInstructEmbeddings
+from langchain_community.embeddings.sentence_transformer import (
+    SentenceTransformerEmbeddings,
+)
 
+
+
+# It will store the embedding on disk
+persist_directory = "db"
 
 
 class MySpecialFunctions:
@@ -10,23 +24,63 @@ class MySpecialFunctions:
     def get_file_text(self, files):
         text = ""
         for file in files:
-            with open(file, 'r') as f:
-                content = f.read()
-                text += content
+            try:
+                with open(file, 'r', encoding='utf-8') as f:
+                    content = f.read()
+                    text += content
+            except Exception as e:
+                print(f"Error reading file {file}: {e}")
+
         return text
+    
+    
+    def get_pdf_text(self, pdf):
+        text = ""
+        for doc in pdf:
+            reader = PdfReader(doc)
+            for page in reader.pages:
+                text += page.extract_text()
+
+        return text
+
+
     
     
     def get_text_chunks(self, text):
         text_siplitter = RecursiveCharacterTextSplitter(
-            chunk_size = 500, 
-            chunk_overlaps= 100,
+            chunk_size = 1000, 
+            chunk_overlap = 200,
             separators=['\n', '\n\n'],
             length_function = len)
-        chunk = text_siplitter.split_documents(text)
+        chunk = text_siplitter.split_text(text)
         return chunk
     
 
-    def get_vectorstore():
+    def get_vectorstore(self, chunks):
+        # embed with OpenAI
+        #embedding = OpenAIEmbeddings()
+        hf_embedding = HuggingFaceEmbeddings()
+
+        # embed with HuggingFace. You replace cpu by cuda
+        # hf_embedding = HuggingFaceInstructEmbeddings(
+        #     model_name="hkunlp/instructor-xl", 
+        #     model_kwargs={"device": "cpu"})
+
+        # create the open-source embedding function
+        embedding_function = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+        
+        # create vectorstore
+        vector_db = Chroma.from_documents(
+            documents = chunks,
+            embedding = hf_embedding,
+            #persist_directory = persist_directory
+        )
+
+        return vector_db #, persist_directory
+
+
+
+
 
         
 
--- RAG/rag.py
+++ RAG/rag.py
@@ -1,22 +1,29 @@
 import streamlit as st 
 from MyFunctions import MySpecialFunctions
+from dotenv import load_dotenv
 
 
 # create an instance of my class
 special_functions_instance = MySpecialFunctions()
 
-def get_text(fine_tuned_text):
-    return special_functions_instance.get_file_text(fine_tuned_text)
+def get_text(external_data ):
+    return special_functions_instance.get_file_text(external_data )
 
+def get_pdf_text(external_data ):
+    return special_functions_instance.get_pdf_text(external_data )
 
+def get_chunks(text):
+    return special_functions_instance.get_text_chunks(text)
 
+def get_vectorstore(text_chunks):
+    return special_functions_instance.get_vectorstore(text_chunks) 
 
 
 
 
 
 def main():
-
+    load_dotenv()
     st.set_page_config(page_title="Generation of Telegram Ads in Amharic", page_icon= ":smile")
     
 
@@ -25,16 +32,24 @@ def main():
         pass
 
     st.markdown("## Ads Generation in Amharic")
-    fine_tuned_text = st.file_uploader(" Upload the generative text from fune-tuning", accept_multiple_files= True)
+    external_data = st.file_uploader(" Upload the generative text from fune-tuning", accept_multiple_files= True)
     if st.button("Retrieval"):
         with st.spinner("Processing"):
             # get files text
-            text = get_text(fine_tuned_text)
-            st.write(text)
+            #text = get_text(external_data )
+            text = get_pdf_text(external_data )
+            #st.write(text)
 
             # get_text_chunks(text)
-            text_chunks = get_text_chunks(text)
+            text_chunks = get_chunks(text)
             st.write(text_chunks)
+
+
+            # get_vectorstore 
+            vectorstore_db = get_vectorstore(text_chunks)
+            #st.write(vectorstore_db)
+            #st.write("--------------")
+            #st.write(persist_directory)
 
 
 
